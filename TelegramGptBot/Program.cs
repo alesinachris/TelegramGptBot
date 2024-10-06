9306d44f40e94741a81fe0ff962f49f73558a6d6
Chris <Chris@DESKTOP-4N92QHM>
2024-10-06 15:40:29 +0700

    Доделан бот без контекста

--- TelegramGptBot/Program.cs
+++ TelegramGptBot/Program.cs
@@ -1,10 +1,130 @@
-﻿namespace TelegramGptBot
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramGptBot
 {
     internal class Program
     {
+        private static ITelegramBotClient _botClient;
+        private static ReceiverOptions _receiverOptions;
+
         static async Task Main(string[] args)
         {
-            
+            // создание объекта клиента для работы с телеграм
+            _botClient = new TelegramBotClient("7701907682:AAG48gxLlhxUW1rkEAZEdVqOyEkJMhZnKWs");
+            // создание объекта настроек получения обновлений
+            _receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = new[] // типы получаемых обновлений
+                {
+                    UpdateType.Message,
+                    UpdateType.CallbackQuery
+                },
+                ThrowPendingUpdates = true,
+            };
+
+            // токен отмены (не используется, просто как заглушка используется)
+            using var cts = new CancellationTokenSource();
+            // старт получения обновлений
+            _botClient.StartReceiving(UpdateHandler, ErrorHandler, _receiverOptions, cts.Token);
+
+            // тест запуска бота
+            var me = await _botClient.GetMeAsync();
+            // вывод результата теста запуска бота
+            Console.WriteLine($"{me.FirstName} запущен!");
+
+            // бесконечное ожидание, чтобы консоль не закрылась
+            await Task.Delay(-1);
+        }
+
+        /// <summary>
+        /// Обработчик обновлений от телеграм
+        /// </summary>
+        /// <param name="botClient"></param>
+        /// <param name="update"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        private static async Task UpdateHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            try
+            {
+                // Если тип сообщения UpdateType.Message, то обрабатываем сообщение
+                if (update.Type == UpdateType.Message)
+                {
+                    // вывод сообщения пользователя
+                    Console.WriteLine(update.Message.Text);
+                    // получение идентификатора чата, из которго пришло сообщение
+                    Chat chat = update.Message.Chat;
+                    // если стартовое сообщение, то выводим приветствие
+                    if(update.Message.Text == "/start")
+                    {
+                        await Start(botClient, chat);
+                    }
+                    // если не стартовое сообщение, то считаем, что это вопрос
+                    else
+                    {
+                        // если сообщение слишком длинное, более 500 символов, то выводим сообщение об ошибке
+                        if (update.Message.Text.Length > 500)
+                        {
+                            await VeryLargeText(botClient, chat);
+                        }
+                        // если сообщение подходящей длины, то отправляем вопрос в VseGPT и выводим ответ
+                        else
+                        {
+                            // получение ответа
+                            string answer = await VseGpt.GetAnswer(update.Message.Text);
+                            // вывод ответа ботом
+                            await botClient.SendTextMessageAsync(chat.Id, answer);
+                            await botClient.SendTextMessageAsync(chat.Id, "Ещё вопрос?");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // если в коде происходит исключение, то выводим в консоль исключение
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Отправка стартового сообщения
+        /// </summary>
+        /// <param name="botClient"></param>
+        /// <param name="chat"></param>
+        /// <returns></returns>
+        private static async Task Start(ITelegramBotClient botClient, Chat chat)
+        {
+            await botClient.SendTextMessageAsync(chat.Id, "Привет, я модель GPT-4o-mini, ваш личный помощник! Задайте ваш вопрос.");
+        }
+
+        /// <summary>
+        /// Отправка сообщения о слишком длинном тексте
+        /// </summary>
+        /// <param name="botClient"></param>
+        /// <param name="chat"></param>
+        /// <returns></returns>
+        private static async Task VeryLargeText(ITelegramBotClient botClient, Chat chat)
+        {
+            await botClient.SendTextMessageAsync(chat.Id, "Слишком длинный вопрос.");
+        }
+
+        private static Task ErrorHandler(ITelegramBotClient botClient, Exception error, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = error switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => error.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
         }
     }
 }
---
+++ TelegramGptBot/VseGpt.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramGptBot
+{
+    internal class VseGpt
+    {
+        public static async Task<string> GetAnswer(string prompt)
+        {
+            // ключ в VseGPT после регистрации
+            string apiKey = "sk-or-vv-78c4ce3f634446da64d5c0f27ffcd7bcb8af0b28f5ac55e691235c5f797c71a6";
+            // базовый url API
+            string baseApiUrl = "https://api.vsegpt.ru/v1/";
+
+            try
+            {
+                HttpClient client = new HttpClient();
+                // добавление заголовка авторизации с токеном
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
+                // список сообщений для отправки, также для хранения контекста беседы
+                List<dynamic> messages = new List<dynamic>();
+                messages.Add(new { role = "user", content = prompt });
+
+                // формирование тела запроса к VseGPT
+                var requestData = new
+                {
+                    model = "openai/gpt-4o-mini",
+                    messages = messages,
+                    temperature = 0.3,
+                    n = 1,
+                    max_tokens = 2000,
+                    extra_headers = new { X_Title = "My App" } // опционально - передача информации об источнике API-вызова
+                };
+
+                // сериализация тела запроса в строку json
+                string jsonRequest = JsonConvert.SerializeObject(requestData);
+                var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+                // запрос ответа
+                HttpResponseMessage response = await client.PostAsync(baseApiUrl + "chat/completions", content);
+
+                // если ответ сервера успешный, то получаем из тела запроса ответ и возвращаем
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    dynamic responseData = JsonConvert.DeserializeObject(jsonResponse);
+                    string responseContent = responseData.choices[0].message.content;
+                    Console.WriteLine("Response: " + responseContent);
+                    return responseContent;
+                }
+                // если ответ сервера не успешный, то возвращаем сообщение об ошибке от сервера
+                else
+                {
+                    return "Error: " + response.ReasonPhrase;
+                }
+            }
+            catch (Exception ex)
+            {
+                // если произошло исключение, то возвращаем текст ошибки
+                return "Exception: " + ex.Message;
+            }
+        }
+    }
+}
